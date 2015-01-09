531f42d56b36815bea205ce44e12a172ae7062ea
Alex Beeken <aeb242@gmail.com>
2015-01-09 20:30:33 +0000

    added results pages, with correct grammar.

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require('sinatra')
 require('sinatra/reloader')
+require('./lib/word_frequency')
 
 # Set port for compatability with Nitrous.IO
 configure :development do
@@ -8,12 +9,22 @@ set :port, 3000 # Not really needed, but works well with the "Preview" menu opti
 end
 
 get('/') do
-  erb(:home)
+  erb(:wf_form)
 end
 
-get('/show_results') do
+get('/wf_result') do
   @input_string = params.fetch('input_string')
   @findme = params.fetch('findme')
-  @results = @input_string.word_frequency(@findme)
-  erb(:home)
+  @final_count = @input_string.word_frequency(@findme)
+  
+  if @final_count == 0
+    @results_message = "There are no instances of the word #{@findme} (<em>not counting typos!!</em>) inside of #{@input_string}."
+  erb(:wf_result)
+  elsif @final_count == 1
+    @results_message = "There is one instance of the word #{@findme} (<em>not counting typos!!</em>) inside of #{@input_string}."
+  erb(:wf_result)
+  else
+    @results_message = "There are #{@final_count} instances of the word \"#{@findme}\" inside of \"#{@input_string}.\""
+  end
+    
 end
