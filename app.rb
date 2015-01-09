6b75fe32b185540e834f66fd2e2c38b14b4658ea
Alex Beeken <aeb242@gmail.com>
2015-01-09 19:56:51 +0000

    got app.rb to be compatible for nitrous testing

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require('sinatra')
-require('sinatra-contrib')
+require('sinatra/reloader')
+
+# Set port for compatability with Nitrous.IO
+configure :development do
+set :bind, '0.0.0.0'
+set :port, 3000 # Not really needed, but works well with the "Preview" menu option
+end
 
 get('/') do
   erb(:home)
