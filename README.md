4a716c0c3d4dab131db36c861755695dc3cc2ebe
Alex Beeken <aeb242@gmail.com>
2015-01-09 16:26:40 -0800

    Added README.md and formatted results page into different vertical divs

---
+++ README.md
@@ -0,0 +1,31 @@
+# Project Name
+
+Word Frequency is a website written with Ruby that counts the number of times a word appears in a block of text.
+
+## Installation
+
+Run 'bundle' in your repository.
+
+To begin hosting, navigate to hosting directory in terminal and run 'ruby app.rb'.
+
+Navigate to 'http://localhost:4567'
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## Credits
+
+Alex Beeken
+
+## License
+
+MIT License
+
+## Bugs
+
+Does not search for words that contain non-alphabetic characters such as an apostrophe in the word "Don't"
--- app.rb
+++ app.rb
@@ -3,11 +3,13 @@ require('sinatra/reloader')
 require('./lib/word_frequency')
 require('./lib/highlight')
 
+=begin
 # Set port for compatability with Nitrous.IO
 configure :development do
 set :bind, '0.0.0.0'
 set :port, 3000 # Not really needed, but works well with the "Preview" menu option
 end
+=end
 
 get('/') do
   erb(:wf_form)
@@ -18,16 +20,6 @@ get('/wf_result') do
   @findme = params.fetch('findme')
   @final_count = (@input_string.word_frequency(@findme))
   @input_string = @input_string.highlight(@findme)
-  
-  if @final_count == 0
-    @results_message = "There are no instances of the word \"#{@findme}\" (<em>not counting typos!!</em>) inside of \"#{@input_string}.\""
 
-  elsif @final_count == 1
-    @results_message = "There is one instance of the word \"#{@findme}\" (<em>not counting typos!!</em>) inside of \"#{@input_string}.\""
-  
-  else
-    @results_message = "There are #{@final_count} instances of the word \"#{@findme}\" inside of \"#{@input_string}.\""
-    
-  end
   erb(:wf_result)
 end
